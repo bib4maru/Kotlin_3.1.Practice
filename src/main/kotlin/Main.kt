eb467f170a82cd1d0a5475601193a7c067ba57a1
bib4maru <kirillosanov@yandex.ru>
2023-11-12 20:47:53 +0300

    Task 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    //Task 1
+    val newEvent = Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.", daypart="Evening", durationInMinutes=15)
+    println(newEvent)
+}
